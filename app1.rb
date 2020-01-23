6d7d24efbc4633cab0c5978bedc46dcc6ddca835
genadyms <genadyms@gmail.com>
2020-01-24 02:38:55 +0300

    Update project

--- app1.rb
+++ app1.rb
@@ -1,24 +1,5 @@
-# frozen_string_literal: true
-
 require 'sinatra'
 
 get '/' do
   erb :index
-end
-
-get '/contacts' do
-  @title = 'Contacts'
-  @message = 'Contacts message'
-  erb :message
-end
-
-post '/' do
-  @login = params[:login]
-  @password = params[:password]
-  if @login == 'admin' && @password == 'secret'
-    erb :welcome
-  else
-    @message = 'Access denied!'
-    erb :index
-  end
 end
