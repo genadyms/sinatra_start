4b9db9964555a3ebc99636b4003393c2f07dd5d8
genadyms <genadyms@gmail.com>
2020-01-24 02:06:13 +0300

    Added welcome, message views

--- app1.rb
+++ app1.rb
@@ -6,7 +6,19 @@ get '/' do
   erb :index
 end
 
+get '/contacts' do
+  @title = 'Contacts'
+  @message = 'Contacts message'
+  erb :message
+end
+
 post '/' do
   @login = params[:login]
-  erb :index
+  @password = params[:password]
+  if @login == 'admin' && @password == 'secret'
+    erb :welcome
+  else
+    @message = 'Access denied!'
+    erb :index
+  end
 end
