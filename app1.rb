637b2783d472a16017bbcb7f3d225c477e5e274d
genadyms <genadyms@gmail.com>
2020-01-24 03:10:09 +0300

    Save data to file

--- app1.rb
+++ app1.rb
@@ -1,5 +1,22 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 
 get '/' do
   erb :index
+end
+
+post '/' do
+  @name = params[:name]
+  @phone = params[:phone]
+  @date_time = params[:date_time]
+
+  @title = 'Thank you'
+  @message = "Dear #{@name}, we'll be waiting for you at #{@date_time}"
+
+  f = File.open 'users.txt', 'a'
+  f.write "User: #{@name}, Phone: #{@phone}, Date and time: #{@date_time}"
+  f.close
+
+  erb :message
 end
