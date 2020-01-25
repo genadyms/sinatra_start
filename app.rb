96e49f046c553c11812c98c82e12153fa6d48437
genadyms <genadyms@gmail.com>
2020-01-25 12:42:16 +0300

    Added bootstrap template

---
+++ app.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require 'rubygems'
+require 'sinatra'
+
+configure do
+  enable :sessions
+end
+
+helpers do
+  def username
+    session[:identity] || 'Hello stranger'
+  end
+end
+
+before '/secure/*' do
+  unless session[:identity]
+    session[:previous_url] = request.path
+    @error = 'Sorry, you need to be logged in to visit ' + request.path
+    halt erb(:login_form)
+  end
+end
+
+get '/' do
+  erb 'Can you handle a <a href="/secure/place">secret</a>?'
+end
+
+get '/login/form' do
+  erb :login_form
+end
+
+post '/login/attempt' do
+  user_name = params['username']
+  user_password = params['userpassword']
+  if user_name == 'admin' && user_password == 'secret'
+    session[:identity] = user_name
+  end
+  where_user_came_from = session[:previous_url] || '/'
+  redirect to where_user_came_from
+end
+
+get '/logout' do
+  session.delete(:identity)
+  erb "<div class='alert alert-message'>Logged out</div>"
+end
+
+get '/secure/place' do
+  erb 'This is a secret place that only <%=session[:identity]%> has access to!'
+end
